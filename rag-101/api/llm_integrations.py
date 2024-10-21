773dd3cf60b3e926f8cbbd8d7b04b9c1409a7bae
nehasaini1608 <164166970+nehasaini1608@users.noreply.github.com>
2024-10-21 12:09:39 +0530

    Update llm_integrations.py

--- rag-101/api/llm_integrations.py
+++ rag-101/api/llm_integrations.py
@@ -1,116 +1,40 @@
-from langchain_community.chat_models import (
-    ChatOpenAI,
-    ChatVertexAI,
-    AzureChatOpenAI,
-    BedrockChat,
-    ChatCohere,
-)
-#from langchain_openai import AzureChatOpenAI
-from langchain_mistralai.chat_models import ChatMistralAI
+from elasticsearch import Elasticsearch
 import os
-import vertexai
-import boto3
 
-LLM_TYPE = os.getenv("LLM_TYPE", "azure")
+ELASTIC_CLOUD_ID = os.getenv("ELASTIC_CLOUD_ID")
+ELASTICSEARCH_URL = os.getenv("ELASTICSEARCH_URL")
+ELASTIC_API_KEY = os.getenv("ELASTIC_API_KEY")
 
 
-def init_openai_chat(temperature):
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    return ChatOpenAI(
-        openai_api_key=OPENAI_API_KEY, streaming=True, temperature=temperature
+
+# TODO move this to central location so this and search_service don't create separate connections
+# Initialize the Elasticsearch client
+es_client = Elasticsearch(
+    hosts=os.getenv('ES_URL', 'http://kubernetes-vm:9200'),
+    # api_key=os.getenv('ES_API_KEY'),
+    basic_auth=(
+        os.getenv('ES_USER', 'elastic'),
+        os.getenv('ES_PASSWORD', 'changeme')
+    ),
+    timeout=90
+)
+
+es_client = Elasticsearch(
+        cloud_id=ELASTIC_CLOUD_ID, api_key=ELASTIC_API_KEY
     )
 
 
-def init_vertex_chat(temperature):
-    VERTEX_PROJECT_ID = os.getenv("VERTEX_PROJECT_ID")
-    VERTEX_REGION = os.getenv("VERTEX_REGION", "us-central1")
-    vertexai.init(project=VERTEX_PROJECT_ID, location=VERTEX_REGION)
-    return ChatVertexAI(streaming=True, temperature=temperature)
+def es_chat_completion(prompt, inference_id):
 
-
-def init_azure_chat(temperature):
-    # OPENAI_VERSION = os.getenv("OPENAI_VERSION")
-    # BASE_URL = os.getenv("OPENAI_BASE_URL")
-    # OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    # OPENAI_ENGINE = os.getenv("OPENAI_ENGINE")
-
-    AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
-    AZURE_OPENAI_API_KEY = os.getenv("AZURE_OPENAI_API_KEY")
-    AZURE_OPENAI_VERSION = os.getenv('AZURE_OPENAI_VERSION')
-    AZURE_OPENAI_DEPLOYMENT = os.getenv('AZURE_OPENAI_DEPLOYMENT')
-
-    # return AzureChatOpenAI(azure_deployment=AZURE_OPENAI_DEPLOYMENT, api_version=AZURE_OPENAI_VERSION, temperature=0,
-    #                 max_tokens=None, timeout=None,
-    #                 max_retries=2,)
-
-    return AzureChatOpenAI(
-        deployment_name=AZURE_OPENAI_DEPLOYMENT,
-        #openai_api_base=AZURE_OPENAI_ENDPOINT,
-        openai_api_version=AZURE_OPENAI_VERSION,
-        openai_api_key=AZURE_OPENAI_API_KEY,
-        streaming=True,
-        temperature=temperature,
+    response = es_client.inference.inference(
+        inference_id = inference_id,
+        task_type = "completion",
+        input = prompt,
+        timeout="90s"
     )
 
 
-def init_bedrock(temperature):
-    AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
-    AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
-    AWS_REGION = os.getenv("AWS_REGION")
-    AWS_MODEL_ID = os.getenv("AWS_MODEL_ID", "anthropic.claude-v2")
-    BEDROCK_CLIENT = boto3.client(
-        service_name="bedrock-runtime",
-        region_name=AWS_REGION,
-        aws_access_key_id=AWS_ACCESS_KEY,
-        aws_secret_access_key=AWS_SECRET_KEY,
-    )
-    return BedrockChat(
-        client=BEDROCK_CLIENT,
-        model_id=AWS_MODEL_ID,
-        streaming=True,
-        model_kwargs={"temperature": temperature},
-    )
+    return response['completion'][0]['result']
 
 
-def init_mistral_chat(temperature):
-    MISTRAL_API_ENDPOINT = os.getenv("MISTRAL_API_ENDPOINT")
-    MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
-    MISTRAL_MODEL = os.getenv("MISTRAL_MODEL", "Mistral-large")
-    kwargs = {
-        "mistral_api_key": MISTRAL_API_KEY,
-        "temperature": temperature,
-    }
-    if MISTRAL_API_ENDPOINT:
-        kwargs["endpoint"] = MISTRAL_API_ENDPOINT
-    if MISTRAL_MODEL:
-        kwargs["model"] = MISTRAL_MODEL
-    return ChatMistralAI(**kwargs)
 
-
-def init_cohere_chat(temperature):
-    COHERE_API_KEY = os.getenv("COHERE_API_KEY")
-    COHERE_MODEL = os.getenv("COHERE_MODEL")
-    return ChatCohere(
-        cohere_api_key=COHERE_API_KEY, model=COHERE_MODEL, temperature=temperature
-    )
-
-
-MAP_LLM_TYPE_TO_CHAT_MODEL = {
-    "azure": init_azure_chat,
-    "bedrock": init_bedrock,
-    "openai": init_openai_chat,
-    "vertex": init_vertex_chat,
-    "mistral": init_mistral_chat,
-    "cohere": init_cohere_chat,
-}
-
-
-def get_llm(temperature=0):
-    if not LLM_TYPE in MAP_LLM_TYPE_TO_CHAT_MODEL:
-        raise Exception(
-            "LLM type not found. Please set LLM_TYPE to one of: "
-            + ", ".join(MAP_LLM_TYPE_TO_CHAT_MODEL.keys())
-            + "."
-        )
-
-    return MAP_LLM_TYPE_TO_CHAT_MODEL[LLM_TYPE](temperature=temperature)
